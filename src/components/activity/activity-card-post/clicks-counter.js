2efaffbeac7cbb5666016301fe8bbda016b8fa17
Antanas <antexas@gmail.com>
2018-11-29 00:45:08 +0200

    working delete button

--- src/components/activity/activity-card-post/clicks-counter.js
+++ src/components/activity/activity-card-post/clicks-counter.js
@@ -1,12 +1,21 @@
-
-var buttonsArray = document.querySelectorAll('.js-clickme');
-
-buttonsArray.forEach(function(elem) {
-    elem.addEventListener("click", function() {
+const socialButtonsArray = document.querySelectorAll('.js-clickme');
+socialButtonsArray.forEach((elem) => {
+    elem.addEventListener("click", () => {
         if (event.target.classList.contains('js-clickme')) {
-            var count = Number(event.target.children[1].textContent) ;
-            count += 1; 
+            let count = Number(event.target.children[1].textContent);
+            count += 1;
             event.target.children[1].innerHTML = count;
         }
     });
+});
+
+const likesDelete = document.querySelector('.js-likes-delete');
+
+likesDelete.addEventListener("click", () => {
+    const deleteNumber = document.querySelector(".js-delete-number")
+    let count = Number(deleteNumber.textContent);
+    if (count > 0) {
+        count -= 1;
+        deleteNumber.innerHTML = count;
+    }
 });
