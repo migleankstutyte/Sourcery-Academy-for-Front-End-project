dd3bc13784f7504f116108665e54ec8f72809c51
Antanas <antexas@gmail.com>
2018-11-28 04:49:27 +0200

    fixed js buttons clicking

---
+++ src/components/activity/activity-card-post/clicks-counter.js
@@ -0,0 +1,13 @@
+
+var buttonsArray = document.querySelectorAll('.js-clickme');
+
+buttonsArray.forEach(function(elem) {
+    elem.addEventListener("click", function() {
+        if (event.target.classList.contains('js-clickme')) {
+            var count = Number(event.target.children[1].textContent) ;
+            count += 1; 
+            event.target.children[1].innerHTML = count;
+            
+        }
+    });
+});
