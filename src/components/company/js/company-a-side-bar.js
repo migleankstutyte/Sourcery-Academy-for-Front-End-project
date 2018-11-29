08fb697a73c8b9b4bb5d1135ba612cf2c734aef4
migleankstutyte <migle.ankstutyte@gmail.com>
2018-11-29 18:30:53 +0200

    Accessibility added

--- src/components/company/js/company-a-side-bar.js
+++ src/components/company/js/company-a-side-bar.js
@@ -54,7 +54,7 @@ function renderSideBarElements() {
     // loop through array
     for (let i = 0; i < arr.length; i++) {
         resultHtml += `
-        <li class="nav-list_item" ${arr[i].id}"> 
+        <li role="menuitem" class="nav-list_item" ${arr[i].id}"> 
             <a href="#" class="nav-list__item-link">${arr[i].title}
             ${
                 arr[i].hasPicture // If array has picture (arrow in side-bar) - show img
@@ -76,7 +76,7 @@ function renderSideBarElementsMobile() {
     // loop through array
     for (let i = 0; i < arr.length; i++) {
         resultHtml += `
-        <li class="list__item" ${arr[i].id}"> 
+        <li role="menuitem" class="list__item" ${arr[i].id}"> 
             <a href="${arr[i].href}">${arr[i].title}
             ${
                 arr[i].hasPicture // If array has picture (arrow in side-bar) - show img
