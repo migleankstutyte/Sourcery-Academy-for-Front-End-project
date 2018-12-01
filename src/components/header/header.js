fe06e46cb2dc43d19a0812f2258df4aa2384ea35
Mantas <mantas.gailius4@gmail.com>
2018-12-01 14:43:17 +0200

    Header without selectable dropdown links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,9 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navigation-list')
-    const navLinks = document.querySelectorAll('.navigation-list-item');
+    const nav = document.querySelector('.header-navigation__list')
+    const navLinks = document.querySelectorAll('.header-navigation__list-item');
     burger.addEventListener('click', ()=>{
-        nav.classList.toggle('nav-active');
-        
+        nav.classList.toggle('header-navigation__list--active');
         navLinks.forEach((link,index)=>{
             if(link.style.animation){
                 link.style.animation = '';
@@ -18,3 +17,24 @@ const navSlide = () => {
 }
 
 navSlide();
+const activeNav= () => {
+var links = document.querySelectorAll('a[href="'+document.URL+'"]');
+console.log(document.URL);
+links.forEach(link => {
+    link.classList.toggle('header-navigation__text--active');
+});
+}
+
+activeNav();
+
+/*const profileDropDown = () => {
+    const icon = document.querySelector('.profile-dropdown-icon');
+    const dropdown = document.querySelector('.profile-dropdown');
+
+    icon.addEventListener('click',()=>{
+        icon.classList.toggle('profile-dropdown-icon--active');
+        dropdown.classList.toggle('profile-dropdown--active');
+    });
+}
+
+profileDropDown();*/
