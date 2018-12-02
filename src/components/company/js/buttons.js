baf2eac873f230e24423949abffc23afb9ffe186
migleankstutyte <migle.ankstutyte@gmail.com>
2018-12-02 13:31:07 +0200

    Added email modal

--- src/components/company/js/buttons.js
+++ src/components/company/js/buttons.js
@@ -1,13 +1,17 @@
 // remove Teams button, when "x" is pressed
-const btn = document.getElementById('fa-times');
-btn.onclick = function () {
-    document.getElementById('teams').remove();
+const btn = document.getElementById("fa-times");
+btn.onclick = function() {
+    document.getElementById("teams").remove();
 };
 
-const pickDate  = document.getElementById("pick-date");
+// Show calendar
+const pickDate = document.getElementById("pick-date");
 const calendar = document.getElementById("calendar");
 
-
-pickDate.addEventListener("click", function(){
-  calendar.classList.toggle("appear");
-}, false);
+pickDate.addEventListener(
+    "click",
+    function() {
+        calendar.classList.toggle("appear");
+    },
+    false
+);
