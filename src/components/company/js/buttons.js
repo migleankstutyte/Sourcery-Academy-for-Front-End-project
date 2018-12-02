a71d98aa5f7aa98536d6e7ddd6336905948cd364
migleankstutyte <migle.ankstutyte@gmail.com>
2018-12-02 12:54:46 +0200

    Some animations added

--- src/components/company/js/buttons.js
+++ src/components/company/js/buttons.js
@@ -3,3 +3,11 @@ const btn = document.getElementById('fa-times');
 btn.onclick = function () {
     document.getElementById('teams').remove();
 };
+
+const pickDate  = document.getElementById("pick-date");
+const calendar = document.getElementById("calendar");
+
+
+pickDate.addEventListener("click", function(){
+  calendar.classList.toggle("appear");
+}, false);
